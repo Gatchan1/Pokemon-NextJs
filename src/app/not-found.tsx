08a47c7dd7b99c27bb074aca8b31326589b97df9
Gatchan1 <r.barrio.f@gmail.com>
2024-12-30 18:11:17 +0100

    fix single quote error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div className='sort-of-center'>Ooops! There's nothing here!</div>
+    <div className='sort-of-center'><p>Ooops! There&apos;s nothing here!</p></div>
   )
 }
